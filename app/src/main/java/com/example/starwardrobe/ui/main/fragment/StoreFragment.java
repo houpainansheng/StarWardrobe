9e7d3f423ef87d26c3d528a40573765652af12a8
tsr1994e09051319 <tpl1844490721@163.com>
2017-04-06 20:47:46 +0800

    测试

--- app/src/main/java/com/example/starwardrobe/ui/main/fragment/StoreFragment.java
+++
@@ -1,8 +0,0 @@
-package com.example.starwardrobe.ui.main.fragment;
-
-/**
- * Created by Administrator on 2017/4/6.
- */
-
-public class StoreFragment {
-}
---
+++ app/src/main/java/com/example/starwardrobe/ui/main/fragment/fashion/FashionFragment.java
@@ -0,0 +1,19 @@
+package com.example.starwardrobe.ui.main.fragment.fashion;
+
+import com.rock.mvplibrary.base.BaseFragment;
+
+/**
+ * Created by Administrator on 2017/4/6.
+ */
+
+public class FashionFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+}
